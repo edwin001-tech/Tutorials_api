f1b0166483e99bca73aafabbbc55126b60c6424c
Gapehood <edwinshitakwa8@gmail.com>
2020-04-19 18:17:36 +0300

    second commit

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,12 +1,21 @@
 
   const mongoose = require('mongoose');
 
-const NoteSchema = mongoose.Schema({
+const schema = mongoose.Schema({
     title: String,
     description: String,
     published: Boolean
 }, {
     timestamps: true
 });
+//If you use this app with a front-end that needs id field instead of _id,
+// you have to override toJSON method that map default object to a custom object.
+schema.method("toJSON", function() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
 
-module.exports = mongoose.model('Tutorial', NoteSchema);
+
+
+module.exports = mongoose.model('Tutorial', schema);
