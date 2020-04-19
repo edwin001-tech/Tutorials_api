d20416aa4d9fe7d5720b95f1a32e9eeae6d426ca
Gapehood <edwinshitakwa8@gmail.com>
2020-04-19 18:24:57 +0300

    second commit

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -9,7 +9,7 @@ const schema = mongoose.Schema({
     timestamps: true
 });
 //If you use this app with a front-end that needs id field instead of _id,
-// you have to override toJSON method that map default object to a custom object.
+// you have to override toJSON method that map default object to a custom object
 schema.method("toJSON", function() {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
